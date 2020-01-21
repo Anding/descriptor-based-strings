0cd9ce58ae40e0c1ab63b297c6d80bf4dd1c9c3c
Andrew Read <andrew81244@outlook.com>
2020-01-21 16:41:29 +0800

    file-operations added to buffers.f

--- buffers.f
+++ buffers.f
@@ -2,45 +2,45 @@
 \ requires strings.f
 
 : $buffer ( n -- s$)
-\ allocate a buffer of n bytes and return the descriptor as a handle
+\ allocate a buffer of n bytes and return the descriptor
 \ buffers are permenant strings 
-	dup allocate THROW						( n addr)
-	swap 0 swap -1 $make					( s$)
+	dup allocate THROW						( n addr)						\ allocate the memory
+	swap 0 swap -1 $make					( s$)							\ make a descriptor
 ;
 
 : $debuffer ( s$ --)
 \ deallocate the buffer associated with string s$
-\ although buffers are permenant strings, deallocate the descriptor
+\ although buffers are permenant strings, also deallocate the descriptor
 	$addr free drop							( s$)
-	dup $.size 0 swap !						( s$) 							\ force a temporary string
+	dup $.size 0 swap !						( s$) 							\ force to be temporary string
 	$drop
 ;
 
 : $copy ( s$ -- s$ r$)
-\ Call allocate to obtain sufficient memory, then copy the character data
+\ allocate to sufficient memory, then copy the character data
 \ referenced by s$ into a fresh buffer and return a new descriptor r$
 \ if s$ references a sub-string, only that portion is copied
 \ r$ is a permenant string
 	>R
-	R@ $.addr @ R@ $.start @ +		( c-addr R:s$)
-	R@ $.len @						( c-addr u R:s$)						\ locate the character data in s$
-	dup $buffer						( c-addr u r$ R:s$)
-	-rot $write						( r$ R:s$)
-	R>								( r$ s$)
+	R@ $.addr @ R@ $.start @ +				( c-addr R:s$)
+	R@ $.len @								( c-addr u R:s$)				\ locate the character data in s$
+	dup $buffer								( c-addr u r$ R:s$)
+	-rot $write								( r$ R:s$)
+	R>										( r$ s$)
 ;	
 
 : $map ( fileid -- s$)
 \ memory map a file to a buffer and return the descriptor
-	( obtain the file size	) 				( fileid size)				
-	dup $buffer >R							( fileid size R:s$)
-	R@ $addr @ nip swap rot					( addr n fileid R:s$)
-	( read 0= THROW	)						( R: s$)
-	R>										( s$)
+	dup file-size THROW drop 				( fileid size)					\ file-size returns a double			
+	dup $buffer >R							( fileid size R:s$)				\ allocate a suitable buffer
+	R@ $addr nip swap rot					( addr n fileid R:s$)
+	read-file THROW							( len R:s$)
+	R@ $.len ! R>							( s$)							\ update len									
 ;
 
 : $demap ( s$ fileid -- s$)
 \ save the contents of the string to fileid
-	>R swap $s R>								( s$ addr n fileid)
-	( write 0= THROW )
+	>R $s R>								( s$ addr n fileid)
+	write-file THROW						( s$)
 ;
 
--- testing_buffers.f
+++ testing_buffers.f
@@ -15,13 +15,21 @@ include buffers.f
 	$= -rot $drop $drop
 ;
 
+: open-test-file ( -- wfileid)
+	s" testing_buffers_temp.txt" r/w create-file THROW	\ will overwite any preexisting file 
+;
+
 cr Tstart
 
 255 $initialize
+open-test-file CONSTANT test-file						\ gforth will handle close-file on bye
 
 T{ 100 $buffer $size swap $debuffer }T 100 ==
 T{ 100 $buffer $len swap $debuffer }T 0 ==
 T{ s" ABCD" $q $copy $T= }T true ==
+T{ s" ABCD" $q test-file $demap $drop }T ==
+0 0 test-file reposition-file
+T{ test-file $map s" ABCD" $q $T= }T true ==
 
 cr Tend
 cr bye
